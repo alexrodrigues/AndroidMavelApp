42b261b7b44a938f1aa996e11e03e4f7e6a7d0fa
Alex Rodrigues <alex.rodrigues.developer@gmail.com>
2019-12-24 10:54:34 -0300

    creating fragments and activies

--- app/src/main/java/com/example/marvelapp/features/detail/DetailActivity.kt
+++ app/src/main/java/com/example/marvelapp/features/detail/DetailActivity.kt
@@ -1,12 +1,13 @@
-package com.example.marvelapp
+package com.example.marvelapp.features.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.marvelapp.R
 
-class MainActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_detail)
     }
 }
---
+++ app/src/main/java/com/example/marvelapp/features/home/fragments/favorites/FavoritesFragment.kt
@@ -0,0 +1,26 @@
+package com.example.marvelapp.features.home.fragments.favorites
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.marvelapp.R
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class FavoritesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favorites, container, false)
+    }
+
+
+}
