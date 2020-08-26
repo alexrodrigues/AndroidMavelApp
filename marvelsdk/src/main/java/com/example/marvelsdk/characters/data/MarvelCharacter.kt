eb81949876dd4d15a3c6310c444ce7c0969b4eb2
Alex Rodrigues <alex.rodrigues.developer@gmail.com>
2020-08-26 08:10:39 -0300

    fixing bug on thumbnail

--- marvelsdk/src/main/java/com/example/marvelsdk/characters/data/MarvelCharacter.kt
+++ marvelsdk/src/main/java/com/example/marvelsdk/characters/data/MarvelCharacter.kt
@@ -5,7 +5,7 @@ data class MarvelCharacter(
     val name: String? = null,
     val description: String? = null,
     val resourceURI: String? = null,
-    val thumbnail: String? = null,
+    val thumbnail: MarvelCharacterThumbnail? = null,
     val comics: MarvelCharacterSummary? = null,
     val series: MarvelCharacterSummary? = null,
     val stories: MarvelCharacterSummary? = null,
